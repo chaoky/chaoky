110d86369d74d545d3d2984b009f669763b6f9d3
chaoky <levimanga@gmail.com>
2025-03-04 07:41:48 -0300

    chore: use do blocks for long styles

--- camp/src/Draw.purs
+++ camp/src/Draw.purs
@@ -12,15 +12,16 @@ import Deku.DOM as D
 import Deku.DOM.Attributes as DA
 import Deku.DOM.Listeners as DL
 import Deku.Do as Deku
-import Misc (css, displayFlex)
+import Misc (displayFlex)
 
 canva :: Nut
 canva = Deku.do
   D.div
-    [ css $ displayFlex
-        *> CSS.flexDirection CSS.row
-        *> squareBorder CSS.borderLeft
-        *> squareBorder CSS.borderTop
+    [ DA.style_ $ render do
+        displayFlex
+        CSS.flexDirection CSS.row
+        squareBorder CSS.borderLeft
+        squareBorder CSS.borderTop
     ]
     (divSquare 10)
 
@@ -35,15 +36,15 @@ square = Deku.do
     [ DL.mouseenter_ \_ -> setHovering true
     , DL.mouseleave_ \_ -> setHovering false
     , DA.style $ hovering <#>
-        \x -> render $ pure unit
-          *> (if x == true then CSS.backgroundColor CSS.black else pure unit)
-          *> squareBorder CSS.borderBottom
-          *> squareBorder CSS.borderRight
-          *> CSS.maxWidth (CSS.px 50.0)
-          *> CSS.maxHeight (CSS.px 50.0)
-          *> CSS.height (CSS.vw 8.0)
-          *> CSS.width (CSS.vw 8.0)
-          *> CSS.cursor Cursor.pointer
+        \h -> render do
+          when h $ CSS.backgroundColor CSS.black
+          squareBorder CSS.borderBottom
+          squareBorder CSS.borderRight
+          CSS.maxWidth (CSS.px 50.0)
+          CSS.maxHeight (CSS.px 50.0)
+          CSS.height (CSS.vw 8.0)
+          CSS.width (CSS.vw 8.0)
+          CSS.cursor Cursor.pointer
     ]
     []
 
