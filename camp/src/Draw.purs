86b63d5b51b57593faf2efe90293733bebb17617
chaoky <levimanga@gmail.com>
2025-03-04 07:31:50 -0300

    feat: improve grid

--- camp/src/Draw.purs
+++ camp/src/Draw.purs
@@ -2,7 +2,6 @@ module Draw where
 
 import Prelude
 
-import CSS ((&))
 import CSS as CSS
 import CSS.Cursor as Cursor
 import Data.Array (range)
@@ -18,33 +17,39 @@ import Misc (css, displayFlex)
 canva :: Nut
 canva = Deku.do
   D.div
-    [ css $ displayFlex *> CSS.flexDirection CSS.row ]
+    [ css $ displayFlex
+        *> CSS.flexDirection CSS.row
+        *> squareBorder CSS.borderLeft
+        *> squareBorder CSS.borderTop
+    ]
     (divSquare 10)
 
+divSquare :: Int -> Array Nut
 divSquare n =
-  repeat n
-    $ D.div [ css $ squareStyle *> CSS.flexDirection CSS.column ]
-    $ repeat n square
+  repeat n $ D.div_ $ repeat n square
 
+square :: Nut
 square = Deku.do
   setHovering /\ hovering <- useState false
   D.div
     [ DL.mouseenter_ \_ -> setHovering true
     , DL.mouseleave_ \_ -> setHovering false
-    , DA.style $ hovering <#> \x -> render $ squareStyle *>
-        if x == true then CSS.backgroundColor CSS.black else pure unit
+    , DA.style $ hovering <#>
+        \x -> render $ pure unit
+          *> (if x == true then CSS.backgroundColor CSS.black else pure unit)
+          *> squareBorder CSS.borderBottom
+          *> squareBorder CSS.borderRight
+          *> CSS.maxWidth (CSS.px 50.0)
+          *> CSS.maxHeight (CSS.px 50.0)
+          *> CSS.height (CSS.vw 8.0)
+          *> CSS.width (CSS.vw 8.0)
+          *> CSS.cursor Cursor.pointer
     ]
     []
 
-squareStyle = do
-  CSS.minHeight (CSS.vw $ 80.0 / 10.0)
-  CSS.minWidth (CSS.vw $ 80.0 / 10.0)
-  CSS.display CSS.flex
-  CSS.borderRight CSS.solid (CSS.px 1.0) CSS.black
-  CSS.borderBottom CSS.solid (CSS.px 1.0) CSS.black
-  CSS.cursor Cursor.pointer
+squareBorder :: forall a. (CSS.Stroke -> CSS.Size CSS.LengthUnit -> CSS.Color -> a) -> a
+squareBorder fn = fn CSS.solid (CSS.px 1.0) CSS.black
 
 repeat :: forall a. Int -> a -> Array a
 repeat n x =
   range 0 n # map (\_ -> x)
-
