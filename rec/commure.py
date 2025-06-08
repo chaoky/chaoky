6fe62cdd75096c398e3460568c607f9b3a791c65
chaoky <levimanga@gmail.com>
2025-06-08 16:01:26 -0300

    final commure thing

--- rec/commure.py
+++ rec/commure.py
@@ -1,9 +1,9 @@
 import asyncio
-from datetime import date
-from itertools import batched
+from datetime import date, timedelta
+from itertools import batched, chain
 import json
 import ssl
-from typing import TypedDict, cast
+from typing import Awaitable, Iterable, TypedDict, cast
 
 
 async def mkLichessRequest(path: str):
@@ -23,8 +23,8 @@ async def mkLichessRequest(path: str):
         parsed = cast(object, json.loads(body))
         return parsed
     except json.JSONDecodeError:
-        text = f"invalid json body from {host}{path}\r\nstatus: {head.decode()}"
-        raise Exception(text)
+        err = f"invalid json body from {host}{path}\r\nstatus: {head.decode()}"
+        raise Exception(err)
 
 
 class LeaderboardUser(TypedDict):
@@ -36,8 +36,8 @@ class Leaderboard(TypedDict):
 
 
 async def getOneLeaderboard(nb: int, perfType: str):
-    url = f"/api/player/top/{nb}/{perfType}"
-    response = await mkLichessRequest(url)
+    path = f"/api/player/top/{nb}/{perfType}"
+    response = await mkLichessRequest(path)
     return cast(Leaderboard, response)
 
 
@@ -47,64 +47,71 @@ class RatingHistory(TypedDict):
 
 
 async def getRatingHistory(username: str):
-    url = f"/api/user/{username}/rating-history"
-    response = await mkLichessRequest(url)
+    path = f"/api/user/{username}/rating-history"
+    response = await mkLichessRequest(path)
     return cast(list[RatingHistory], response)
-
-
-async def mkBunchRequests():
-    leaderboard = await getOneLeaderboard(50, "classical")
-    eachRating = (getRatingHistory(user["username"]) for user in leaderboard["users"])
-    chunks = (asyncio.gather(*chunk) for chunk in batched(eachRating, 10))
-
-    async def printPlayer(c: asyncio.Future[list[list[RatingHistory]]]):
-        for things in await c:
-            print(", ".join([t["name"] for t in things]))
-
-    [await printPlayer(chunk) for chunk in chunks]
 
 
 def mkRatingHistoryByDay(history: list[RatingHistory], today: date, perfType: str):
     ratings = next((h for h in history if h["name"] == perfType))
     last30days: list[int] = []
-    end = len(ratings["points"]) - 1
+    current = len(ratings["points"]) - 1
 
-    for i in range(end, 0, -1):
-        year, month, day, rating = ratings["points"][i]
-        scoreDate = date(year, month + 1, day + 1)
-        if today < scoreDate:
-            continue
-
-        timeElapsed = today - scoreDate
-        missingDays = timeElapsed.days - len(last30days)
-        print(scoreDate, rating, timeElapsed.days, missingDays)
-        for d in range(missingDays):
-            last30days.append(rating)
-
-        if len(last30days) == 30:
+    while True:
+        if current < 0:
             break
 
-        if len(last30days) > 30:
-            raise Exception("how")
+        year, month, day, rating = ratings["points"][current]
+        scoreDate = date(year, month + 1, day)
+        if today < scoreDate:
+            current -= 1
+            continue
+
+        last30days.append(rating)
+
+        timeElapsed = today - scoreDate
+        if timeElapsed.days < len(last30days) - 1:
+            current -= 1
+
+        if len(last30days) == 31:
+            break
 
     return last30days
+
+
+async def buffered10[T](xs: Iterable[Awaitable[T]]):
+    async def print_gather(x: Iterable[Awaitable[T]]):
+        print("fetching 10...")
+        return await asyncio.gather(*x)
+
+    chunks = [await print_gather(x) for x in batched(xs, 10)]
+    return chain(*chunks)
 
 
 # Warmup: List the top 50 classical chess players. Just print their usernames.
 def print_top_50_classical_players() -> None:
     leaderboard = asyncio.run(getOneLeaderboard(50, "classical"))
-    print(leaderboard["users"])
+    print(",".join(user["username"] for user in leaderboard["users"]))
 
 
 # Part 2: Print the rating history for the top chess player in classical chess for the las t 30 calendar days.
 # This can be in the format: username, {today-29: 990, today-28: 991, etc}
 def print_last_30_day_rating_for_top_player() -> None:
-    today = date(2025, 5, 5)
-    leaderboard = asyncio.run(getOneLeaderboard(1, "classical"))
-    top1 = leaderboard["users"][0]["username"]
-    history = asyncio.run(getRatingHistory(top1))
-    byDay = mkRatingHistoryByDay(history, today, "Classical")
-    print(top1, byDay)
+    async def do():
+        today = date(2025, 5, 30)
+        leaderboard = await getOneLeaderboard(1, "classical")
+        top1 = leaderboard["users"][0]["username"]
+        history = await getRatingHistory(top1)
+        ratingByDay = mkRatingHistoryByDay(history, today, "Classical")
+        print(
+            top1,
+            ",".join(
+                f"\r\n{today - timedelta(days=i)} : {rating}"
+                for i, rating in enumerate(ratingByDay)
+            ),
+        )
+
+    asyncio.run(do())
 
 
 # Part 3: Create a CSV that shows the rating history for each of these 50 players, for the last 30 days.
@@ -117,7 +124,24 @@ def print_last_30_day_rating_for_top_player() -> None:
 # bob,1231,1158,1250,...,1290
 # notagm,900,900,900,...,900
 def generate_rating_csv_for_top_50_classical_players() -> None:
-    pass
+    async def do():
+        today = date(2025, 5, 30)
+        leaderboard = await getOneLeaderboard(50, "classical")
+        ratings = await buffered10(
+            getRatingHistory(user["username"]) for user in leaderboard["users"]
+        )
 
+        head = chain(
+            ["username"], (str(today - timedelta(days=i)) for i in range(30, -1, -1))
+        )
+        with open("out.csv", "w+") as f:
+            _ = f.write(",".join(head))
+            for user, rating in zip(leaderboard["users"], ratings):
+                ratingByDay = mkRatingHistoryByDay(rating, today, "Classical")
+                ratingByDay.reverse()
+                format = f"\n{user['username']},{','.join(str(rating) for rating in ratingByDay)}"
+                _ = f.write(format)
 
-print_last_30_day_rating_for_top_player()
+        print("wrote to out.csv")
+
+    asyncio.run(do())
